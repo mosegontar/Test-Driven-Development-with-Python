80093f73b7d621f439714fbc4d0e9cbea9a81284
Alex Gontar <mosegontar@gmail.com>
2016-12-24 16:02:30 -0500

    Simplify unit tests and create new test class to test creation of a new list; add new_list view function

--- lists/views.py
+++ lists/views.py
@@ -5,13 +5,15 @@ from lists.models import Item
 def home_page(request):
     """Home page view function"""
 
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 def view_list(request):
     """View list view function"""
-    
+
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    
+    Item.objects.create(text=request.POST['item_text'])    
+    return redirect('/lists/the-only-list-in-the-world/')
