d139b336790d0fa0f63e451dac609cb5a48f4d45
Alex Gontar <mosegontar@gmail.com>
2017-01-14 09:17:50 -0500

    Add functional test for click event listner to hide error message and add click event listener to lists.js

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -47,7 +47,7 @@ class ItemValidationTest(FunctionalTest):
         error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this in your list")
 
-    def test_error_messages_are_cleared_on_input(self):
+    def test_error_messages_are_cleared_on_entered_input(self):
         # Edith starts a list and causes a validation error
         self.browser.get(self.server_url)
         self.get_item_input_box().send_keys('Banter too thick\n')
@@ -63,3 +63,20 @@ class ItemValidationTest(FunctionalTest):
         # She is pleased to see that the error message disappears
         error = self.get_error_element()
         self.assertFalse(error.is_displayed())
+
+    def test_error_messages_are_cleared_on_clicked_input_ement(self):
+        # Edith starts a list and causes a validation error
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Banter too thick\n')
+        self.check_for_row_in_list_table('1: Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick\n')
+
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # She clicks on the input box to clear the error
+        self.get_item_input_box().click()
+        
+        # She is pleased to see that the error message disappears
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())        
