25fca170f067c8d17f59ad57b1184ce3b9350656
Alex Gontar <mosegontar@gmail.com>
2017-03-05 14:12:48 -0500

    Add accounts.Token model, pass TokenModelTest

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,12 @@
+import uuid
+
 from django.db import models
+
+
+class Token(models.Model):
+    email = models.EmailField()
+    uid = models.CharField(default=uuid.uuid4, max_length=40)
+
 
 class User(models.Model):
     email = models.EmailField(primary_key=True)
